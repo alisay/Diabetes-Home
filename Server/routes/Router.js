c2ac44a79d283a5248fe6d47b4ab522cdf7115fa
Alisa <alisa.blakeney@gmail.com>
2022-04-06 12:15:11 +1000

    revise folder structure

--- Server/routes/Router.js
+++ Server/routes/Router.js
@@ -2,7 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const app = express();
+const port = 3000
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
