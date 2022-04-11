33f9e0a05af9ad8c1dc430be5003738b010106ef
hoang <minhhoangp@student.unimelb.edu.au>
2022-04-12 00:09:00 +1000

    implement handlebars

---
+++ Client/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('hbs', exphbs.engine({
+    defaultlayout: 'main',
+    extname: 'hbs',
+    helpers: {
+    }
+})) 
+
+app.set('view engine', 'hbs')
+
+// index.html
+app.get('/', (req, res) => { 
+    res.render('index')
+})
+
+app.get('/aboutDiabetes', (req, res) => {
+    res.render('aboutDiabetes', {})
+})
+
+app.get('/aboutWebsite', (req, res) => {
+    res.render('aboutWebsite', {})
+})
+
+// default route to handle errors
+app.get('*', (req, res) => {
+    res.status(404).send('<p> invalid request </p>')
+}) 
+
+app.listen(8080, () => {
+    console.log('website listening for request ...')
+})
