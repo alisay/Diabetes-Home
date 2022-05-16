64d1e4799148a9e1afa98baa24068f8ed53899d3
unrealintegers <47798345+unrealintegers@users.noreply.github.com>
2022-05-16 11:49:04 +1000

    implement measurement posting

--- models/measurements.js
+++ models/measurements.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 const { Schema, ObjectId, model } = mongoose;
 
 const MeasurementsSchema = Schema({
-    metadata: { type: ObjectId, index: true },
-    glucose: { measurement: Number, comment: String },
-    weight: { measurement: Number, comment: String },
-    insulin: { measurement: Number, comment: String },
-    steps: { measurement: Number, comment: String },
+    metadata: { 
+        user: ObjectId,
+        type: { type: String },
+        index: true
+    },
+    
+    measurement: Number, 
+    comment: String,
     timestamp: Date,
 },
     {
