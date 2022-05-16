281e32e612959defa4f6a4c73dfd4d5e1e877147
unrealintegers <47798345+unrealintegers@users.noreply.github.com>
2022-05-16 22:59:11 +1000

    data table

--- controllers/clinicianController.js
+++ controllers/clinicianController.js
@@ -1,6 +1,4 @@
 import { getClinician, getPatient, getPatientId, getPatientData } from "../dbutils.js";
-import handlebars from "handlebars";
-import fs from "fs";
 
 export async function clinicianDashboard(req, res) {
     const user = await getClinician("chrissi");
@@ -16,8 +14,34 @@ export async function clinicianDashboard(req, res) {
 export async function patientData(req, res) {
     const patient = await getPatient("PatTap");
     const data = await getPatientData(patient._id, "glucose");
+    const datapoints = data.map(e => e.measurement);
+
+    // Chart constants
+    const { low, high } = patient.metrics["glucose"].threshold;
+    const min = Math.min(low, ...datapoints);
+    const max = Math.max(high, ...datapoints);
+    const ymin = min - (max - min) * 0.2;
+    const ymax = max + (max - min) * 0.2;
+
+    // Table Contents
+    const dataDays = new Object();
+    data.forEach(e => {
+        e.timestamp.setHours(0, 0, 0, 0);
+        dataDays[e.timestamp.toLocaleDateString()] = [e.measurement, e.comment];
+    });
+    const allDays = new Object();
+    new Array.from(Array(28).keys()).forEach(idx => {
+        const d = new Date();
+        d.setDate(d.getDate() - idx);
+        d.setHours(0, 0, 0, 0);
+        allDays[d.toLocaleDateString()] = [];
+    });
+    // console.log(dataDays);
+    const table = {...allDays, ...dataDays};
 
     res.render('patientData', {
-        data: data.map(e => e.measurement)
+        patient, type: "Glucose", datapoints,
+        low, high, ymin, ymax,
+        table
     });
 }
--- dbutils.js
+++ dbutils.js
@@ -15,12 +15,12 @@ export function getPatient(username) {
 export function getPatientData(_id, type, timeframe = 28) {
     const date = new Date();
     date.setHours(0, 0, 0, 0);
-    date.setDate(date.getDate() - timeframe);
+    date.setDate(date.getDate() - timeframe + 1);  // +1 as today is the last day
 
     return models.Measurements.find({
         metadata: { user: _id, type },
-        // timestamp: { $gte: date }
-    }).lean();
+        timestamp: { $gt: date }
+    }).sort({ timestamp: 1 }).lean();
 }
 
 // Later we will want to make a task for this instead of fetching top 5 from DB each time
