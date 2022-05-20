67e9cf638284c681fd117269767bb72eb6bcc8b5
unrealintegers <47798345+unrealintegers@users.noreply.github.com>
2022-05-20 12:06:18 +1000

    remove unused measurement controller and routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 export { default as authRouter } from "./authRouter.js";
 export { default as clinicianRouter } from "./clinicianRoutes.js";
-export { default as measurementRouter } from "./measurementRoutes.js";
 export { default as pageRouter } from "./pageRoutes.js";
 export { default as patientRouter } from "./patientRoutes.js";
