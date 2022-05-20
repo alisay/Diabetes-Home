7e885eeb6149fb57697751543d69bff258f34dc5
unrealintegers <47798345+unrealintegers@users.noreply.github.com>
2022-05-20 14:41:10 +1000

    implement viewHistory route

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,7 +1,11 @@
-import { getPatient, getPatientId, getLeaderboard } from '../dbutils.js';
+import { format } from 'date-fns';
+import { UNITS, TYPES } from '../models/index.js';
+import { getPatient, getPatientId, getPatientData, getLeaderboard } from '../dbutils.js';
 
 function formatMeasurements(user) {
-    for (const measurement of user?.metrics.keys()) {
+    if (user?.metrics == null) return {};
+
+    for (const measurement of Object.keys(user?.metrics)) {
         user.metrics[measurement].unit = UNITS[measurement];
     }
     return user.metrics;
@@ -33,7 +37,7 @@ export async function patientDashboard(req, res) {
 export async function clinicianDashboard(req, res) {
     const { user } = req;
     console.log(user);
-    if(user.patients){
+    if(user.patients) {
         user.patients = await Promise.all(user.patients.map(
             async (p) => await getPatientId(p),
         ));    
@@ -47,3 +51,64 @@ export async function clinicianDashboard(req, res) {
     });
 }
 
+export async function viewHistory(req, res) {
+    const user = await getPatient('PatTap');
+    const measurements = formatMeasurements(user);
+
+    res.render('viewHistory', { 
+        measurements, 
+        css: 'stylesheets/viewHistory.css' 
+    });
+}
+
+export async function viewPatient(req, res) {
+    if (req.user == null) {
+        return res.redirect('/');
+    }
+
+    const patient = await getPatient(req.params.username);
+
+    // Check types
+    const trackingTypes = TYPES.filter((t) => patient.metrics[t] != null);
+    const type = trackingTypes[0];
+    const data = new Map();
+    for (const t of trackingTypes) {
+        data.set(t, await getPatientData(patient._id, t));
+    }
+    const datapoints = data.get(type).map((e) => e.measurement);
+
+    // Chart constants
+    const { low, high } = patient.metrics.glucose.threshold;
+    const min = Math.min(low, ...datapoints);
+    const max = Math.max(high, ...datapoints);
+    const ymin = min - (max - min) * 0.2;
+    const ymax = max + (max - min) * 0.2;
+    const chart = {
+        min, max, ymin, ymax,
+    };
+
+    // Table Contents
+    const allDays = {};
+    Array.from(Array(28).keys()).forEach((idx) => {
+        const d = new Date();
+        d.setDate(d.getDate() - idx);
+        allDays[format(d, 'dd/MM')] = Object.fromEntries(TYPES.map((e) => [e, {}]));
+    });
+    data.forEach((entries, type) => entries.forEach((e) => {
+        allDays[format(e.timestamp, 'dd/MM')][type].measurement = e.measurement;
+        allDays[format(e.timestamp, 'dd/MM')][type].comment = e.comment;
+    }));
+
+    const today = format(new Date(), 'dd/MM');
+
+    res.render('viewPatient', {
+        patient,
+        type: 'Glucose',
+        trackingTypes,
+        datapoints,
+        chart,
+        allDays,
+        today,
+        notes: {},
+    });
+}
